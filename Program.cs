843bb7a7e635bcf37e395114c6f87c8184a88b33
VasiliiChibisov <chibisowasya@gmail.com>
2022-03-07 11:57:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Vasilii";
+            byte MyAge = 29;
+            bool HaveIApet = true;
+            double MyShoeSize = 45;
+
+            Console.WriteLine("My name is {0}", MyName);
+            Console.WriteLine("MyAge {0}", MyAge);
+            Console.WriteLine("Do I have a pet? {0}", HaveIApet);
+            Console.WriteLine("My shoe size is {0}", MyShoeSize);
+            Console.ReadKey();
+        }
+    }
+}
